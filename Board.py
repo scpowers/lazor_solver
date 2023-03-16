aa110b49503f661af5393b1bdb89b77c1a531818
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-03-16 15:40:35 -0400

    [board] added laser attributes

--- Board.py
+++ Board.py
@@ -1,11 +1,16 @@
 
 class Board:
 
-    def __init__(self, initial_board):
-        self.board = initial_board
+    def __init__(self, initial_board, laser_pos, laser_dir):
+        self.board = initial_board  # initial config of board (with everything on it)
+        self.laser_pos = laser_pos  # [x, y] position of laser source on grid where cells are 3x3 across
+        self.laser_dir = laser_dir  # [dx, dy] direction of laser source on grid where cells are 3x3 across
 
     # TODO: modify_board - adjust board configuration
 
     # TODO: get_laser_path - compute and return sequence of points the laser visits
+    def laser_path(self):
+        # convert board into grid where cells are 3x3 across
+        pass
 
     # TODO: render_board - draw board config + laser as grid image and save it
