472dcb66ff16884ac03d8a77f0d46d36602fdc12
basementcrusader <60239242+basementcrusader@users.noreply.github.com>
2023-03-16 15:58:01 -0400

    moved bff

--- parser.py
+++ parser.py
@@ -1,9 +1,14 @@
+import os
+
 def openBFF(filePointer: str="dark_1.bff"):
     
     #open the file
-    file=open(filePointer,"r").read()
+    file=open(filePointer,'r').read()
 
     print(file)
 
+#os.getcwd()
+file_path = os.getcwd() + '\dark_1.bff'
+print(file_path)
+openBFF(file_path)
 
-openBFF()
