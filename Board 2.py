d780675c72b6359cb9ab91eefb6a827657797614
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-03-17 18:16:49 -0400

    [laser path solver] broke out a number of blocks into separate static methods to clean it up, switched to using dict for total visited points across sources

--- Board 2.py
+++ Board 2.py
@@ -12,7 +12,7 @@ class Board:
 
     # TODO: get_laser_path - compute and return sequence of points the laser visits
     def get_laser_path(self):
-        # convert board into grid where cells are 3x3 across
+        # convert board into grid using same convention as .bff file
         new_board = np.zeros((2*len(self.board) + 1, 2*len(self.board) + 1))
         for i, row in enumerate(self.board):
             for j, cell in enumerate(row):
@@ -25,12 +25,13 @@ class Board:
         new_board = np.transpose(new_board)
         print(f'new board:\n {new_board}')
 
-        # check if a given position is within the gridded board
-        def pos_check(coord_board, coords):
-            return 0 <= coords[0] < coord_board.shape[0] and 0 <= coords[1] < coord_board.shape[1]
+        # initialize dict where each key is a laser initial position/direction pair (unique) and
+        # each value is a list of visited coordinates in order [[x1, y1], [x2, y2]]
+        total_visited_pts = {}
+        for i, pos in enumerate(self.laser_pos):
+            total_visited_pts[(tuple(pos), tuple(self.laser_dir[i]))] = []
 
-        # initialize list of visited points across all laser sources
-        total_visited_pts = []
+        # now compute the traveled path for each laser source
         for i, pos in enumerate(self.laser_pos):
             # initialize stack, build from starting point, backtrack if you exit grid or hit opaque block,
             # only able to continue after backtracking if another route exists because of a refraction block
@@ -41,47 +42,23 @@ class Board:
             # keep going until you've emptied the stack or you hit the upper bound on path length
             # (prevents infinite loops)
             while len(laser_path) > 0 and iter < max_path_length:
-                # add most recent position to total visited pts if it's unique and within the board
+                # add most recent position to total visited pts for this laser source if it's within the board
                 latest_pos = laser_path[-1]
-                if pos_check(new_board, latest_pos) and latest_pos not in total_visited_pts:
-                    total_visited_pts.append(latest_pos)
+                if pos_check(new_board, latest_pos):
+                    total_visited_pts[(tuple(pos), tuple(self.laser_dir[i]))].append(latest_pos)
 
-                # compute next position/direction
-                # case 1: latest position is within a vertical slice between cells
-                if latest_pos[0] % 2 == 0:
-                    if direction[0] > 0:
-                        next_relevant_center_coords = [latest_pos[0] + 1, latest_pos[1]]
-                    else:
-                        next_relevant_center_coords = [latest_pos[0] - 1, latest_pos[1]]
-                # case 2: latest position is within a horizontal slice between cells
-                else:
-                    if direction[1] > 0:
-                        next_relevant_center_coords = [latest_pos[0], latest_pos[1] + 1]
-                    else:
-                        next_relevant_center_coords = [latest_pos[0], latest_pos[1] - 1]
+                # get the coordinates of the next relevant cell center (relevant for path behavior at next step)
+                next_relevant_center_coords = get_next_relevant_cell_center(latest_pos, direction)
 
                 # check cell behavior at the next relevant center
-                # but first: check that it's actually a cell
-                if not pos_check(new_board, next_relevant_center_coords):
-                    print('next relevant center is off the grid...end of this laser path')
-                    break  # backtracking not implemented yet
-
-                # case 1: the next relevant cell is empty
-                if new_board[next_relevant_center_coords[0], next_relevant_center_coords[1]] == 0:
-                    next_pos = [latest_pos[0] + direction[0], latest_pos[1] + direction[1]]  # move normally by one step
-                    next_direction = direction  # direction is unchanged
-                # case 2: the next relevant cell is a reflective cell, so change direction
-                elif new_board[next_relevant_center_coords[0], next_relevant_center_coords[1]] == 1:
-                    print('encountered a reflective cell')
-                    next_pos = latest_pos  # position is unchanged
-                    # if you're in a vertical slice between cells, just changing dx
-                    if latest_pos[0] % 2 == 0:
-                        next_direction = [-1 * direction[0], direction[1]]
-                    else:
-                        next_direction = [direction[0], -1 * direction[1]]
-                else:
-                    print('cell type not supported yet')
+                should_path_end = should_laser_path_end(new_board, next_relevant_center_coords)
+                if should_path_end:
+                    print('apparently the path either went off the grid or hit an opaque block')
                     break
+
+                # get the position and direction of the next step in the laser path
+                next_pos, next_direction = get_next_laser_pos_dir(new_board, next_relevant_center_coords, latest_pos,
+                                                                  direction)
 
                 # update
                 laser_path.append(next_pos)
@@ -91,11 +68,66 @@ class Board:
         # store visited points as attribute
         self.laser_visited_pts = total_visited_pts
 
+
     # TODO: render_board - draw board config + laser as grid image and save it
     # Maranda
     def render_board():
 
         return []
+
+
+# check if a given position is within the gridded board
+def pos_check(coord_board, coords):
+    return 0 <= coords[0] < coord_board.shape[0] and 0 <= coords[1] < coord_board.shape[1]
+
+
+# get next relevant cell center
+def get_next_relevant_cell_center(latest_pos, direction):
+    # case 1: latest position is within a vertical slice between cells
+    if latest_pos[0] % 2 == 0:
+        if direction[0] > 0:
+            next_relevant_center_coords = [latest_pos[0] + 1, latest_pos[1]]
+        else:
+            next_relevant_center_coords = [latest_pos[0] - 1, latest_pos[1]]
+    # case 2: latest position is within a horizontal slice between cells
+    else:
+        if direction[1] > 0:
+            next_relevant_center_coords = [latest_pos[0], latest_pos[1] + 1]
+        else:
+            next_relevant_center_coords = [latest_pos[0], latest_pos[1] - 1]
+    return next_relevant_center_coords
+
+
+# get next position and direction of the laser path
+def get_next_laser_pos_dir(new_board, next_relevant_center_coords, latest_pos, direction):
+    next_relevant_cell_val = new_board[next_relevant_center_coords[0], next_relevant_center_coords[1]]
+    # case 1: the next relevant cell is empty or a hole
+    if next_relevant_cell_val == 0 or next_relevant_cell_val == 5:
+        next_pos = [latest_pos[0] + direction[0], latest_pos[1] + direction[1]]  # move normally by one step
+        next_direction = direction  # direction is unchanged
+    # case 2: the next relevant cell is a reflective cell, so change direction
+    elif next_relevant_cell_val == 1:
+        print('encountered a reflective cell')
+        next_pos = latest_pos  # position is unchanged
+        # if you're in a vertical slice between cells, just changing dx
+        if latest_pos[0] % 2 == 0:
+            next_direction = [-1 * direction[0], direction[1]]
+        else:
+            next_direction = [direction[0], -1 * direction[1]]
+    else:
+        raise ValueError(f'cell type {next_relevant_cell_val} not supported yet')
+
+    return next_pos, next_direction
+
+
+# helper method to determine whether the next relevant cell implies that the laser path should end here
+def should_laser_path_end(new_board, next_relevant_center_coords):
+    # just care whether the next cell is off the board OR it's an opaque cell
+    if not pos_check(new_board, next_relevant_center_coords) or new_board[next_relevant_center_coords[0],
+                                                                          next_relevant_center_coords[1]] == 5:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     formatted_board = [[0, 0, 0, 1],
