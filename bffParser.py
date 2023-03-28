c6834534880b632c939ee046c76d7cbad47298ad
basementcrusader <60239242+basementcrusader@users.noreply.github.com>
2023-03-28 18:53:04 -0400

    Clean up, grid is presently a str list

--- bffParser.py
+++ bffParser.py
@@ -22,10 +22,6 @@ def openBFF(filePointer: str = "dark_1.bff"):
     # open the file
     file = open(filePointer, 'r').read()
 
-    # print(file)
-
-    # print(len(file))
-
     # the length of GRID START is 11 characters, hence I add 11 charactes to the index it was first found
     gridStart = 11+file.find("GRID START")
     gridEnd = file.find("GRID STOP")
@@ -40,16 +36,11 @@ def openBFF(filePointer: str = "dark_1.bff"):
 
     lineSplitFile = file.strip().splitlines()
 
-    # print(lineSplitFile)
     # the line preceding the one where grid start was identified
     startGridLine = 1+lineSplitFile.index("GRID START")
 
-    # print(startGridLine)
-
     # the stop grid line is the line before the GRID STOP string
     stopGridLine = lineSplitFile.index("GRID STOP")-1
-    # print(stopGridLine)
-    # print(lineSplitFile[stopGridLine])
 
     # iterate the index from grid start line to grid end line plus 1 to include in range
     gridString = lineSplitFile[startGridLine:stopGridLine+1]
@@ -60,10 +51,11 @@ def openBFF(filePointer: str = "dark_1.bff"):
     # lambda function below splits a given line into a list of values
     def lineList(gridLine): return gridLine.split()
 
-    # print(lineList(gridString[0]))
-
     # cast result of map function appied to gridString and assign as grid
     grid = list(map(lineList, gridString))
+
+    ###CONVERSION OF STRING GRID TO NUMERICAL GRID
+    
 
     # list of list storing the laser origin and tragectory, for now is initialized as an empty string to inform python that it has a job to do
     laserList = []
@@ -72,6 +64,7 @@ def openBFF(filePointer: str = "dark_1.bff"):
     # list of list storing the points the lasers must pass through
     pointGoalList = []
 
+    ###BLOCK, POINT, AND LASER PARSING###
     # iterate from the end of the grid to the end of the file to avoid capturing values within the grid string
     for line in lineSplitFile[stopGridLine:]:
         # check for lazor trajectory
