0169db27facfa9e100896af39d799bbbe2bea231
basementcrusader <60239242+basementcrusader@users.noreply.github.com>
2023-03-28 15:45:05 -0400

    grid construction

--- bffParser.py
+++ bffParser.py
@@ -6,28 +6,48 @@ def openBFF(filePointer: str = "dark_1.bff"):
     # open the file
     file = open(filePointer, 'r').read()
 
-    print(file)
+    #print(file)
 
-    print(len(file))
+    #print(len(file))
+    
     # the length of GRID START is 11 characters, hence I add 11 charactes to the index it was first found
     gridStart = 11+file.find("GRID START")
     gridEnd = file.find("GRID STOP")
 
-    gridString = file[gridStart:gridEnd].strip()
-    grid=gridString.split('\n')
+    #gridString = file[gridStart:gridEnd].strip()
+    #grid=gridString.split('\n')
 
-    print(gridString)
-    print(grid)
+    #print(gridString)
+    #print(grid)
 
     ##alternative approach
 
     lineSplitFile=file.strip().splitlines()
     print(lineSplitFile)
-    startline=lineSplitFile.index("GRID START")
-    print(startline)
+    #the line preceding the one where grid start was identified 
+    startGridLine=1+lineSplitFile.index("GRID START")
+    print(startGridLine)
 
+    #the stop grid line is the line before the GRID STOP string
+    stopGridLine=lineSplitFile.index("GRID STOP")-1
+    print(stopGridLine)
+    print(lineSplitFile[stopGridLine])
+
+    #iterate the index from grid start line to grid end line plus 1 to include in range
+    gridString=lineSplitFile[startGridLine:stopGridLine+1]
+    print(gridString)
+    #define grid as an empty list of lists
+    grid=[[]]
+    
+    #lambda function below splits a given line into a list of values
+    lineList= lambda gridLine: gridLine.split() 
+    print(lineList(gridString[0]))
+
+    #grid
+
+    
 
 # os.getcwd()
 file_path = os.getcwd() + '\dark_1.bff'
-print(file_path)
+#print(file_path)
 openBFF(file_path)
