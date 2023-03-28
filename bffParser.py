0cf6430a2c17b0b7089db4985d3683814d27d642
basementcrusader <60239242+basementcrusader@users.noreply.github.com>
2023-03-28 18:41:33 -0400

    Parser for grid, laser, point goals, and blocks

--- bffParser.py
+++ bffParser.py
@@ -1,4 +1,20 @@
+'''
+@Author: Nick Bruns
+@Created: 3/14/2023
+Parses a BFF board file for the lazors mobile game
+'''
+
 import os
+
+# define values for parser usage later
+FREE = 0  # o block
+REFLECTIVE = 1  # A block
+REFRACTIVE = 2  # B block
+OPAQUE = 3  # C block
+HOLE = 4  # x block
+FIXED_REFLECTIVE = 5
+FIXED_REFRACTIVE = 6
+FIXED_OPAQUE = 7
 
 
 def openBFF(filePointer: str = "dark_1.bff"):
@@ -6,78 +22,116 @@ def openBFF(filePointer: str = "dark_1.bff"):
     # open the file
     file = open(filePointer, 'r').read()
 
-    #print(file)
+    # print(file)
 
-    #print(len(file))
-    
+    # print(len(file))
+
     # the length of GRID START is 11 characters, hence I add 11 charactes to the index it was first found
     gridStart = 11+file.find("GRID START")
     gridEnd = file.find("GRID STOP")
 
-    #gridString = file[gridStart:gridEnd].strip()
-    #grid=gridString.split('\n')
+    # gridString = file[gridStart:gridEnd].strip()
+    # grid=gridString.split('\n')
 
-    #print(gridString)
-    #print(grid)
+    # print(gridString)
+    # print(grid)
 
-    ##alternative approach
+    # alternative approach
 
-    lineSplitFile=file.strip().splitlines()
-    
-    #print(lineSplitFile)
-    #the line preceding the one where grid start was identified 
-    startGridLine=1+lineSplitFile.index("GRID START")
-    
-    #print(startGridLine)
+    lineSplitFile = file.strip().splitlines()
 
-    #the stop grid line is the line before the GRID STOP string
-    stopGridLine=lineSplitFile.index("GRID STOP")-1
-    #print(stopGridLine)
-    #print(lineSplitFile[stopGridLine])
+    # print(lineSplitFile)
+    # the line preceding the one where grid start was identified
+    startGridLine = 1+lineSplitFile.index("GRID START")
 
-    #iterate the index from grid start line to grid end line plus 1 to include in range
-    gridString=lineSplitFile[startGridLine:stopGridLine+1]
-    
-    #print(gridString)
-    
-    #define grid as an empty list of lists
-    grid=[[]]
-    
-    #lambda function below splits a given line into a list of values
-    lineList= lambda gridLine: gridLine.split() 
-    
-    #print(lineList(gridString[0]))
+    # print(startGridLine)
 
-    #cast result of map function appied to gridString and assign as grid  
-    grid=list(map(lineList, gridString))
-    print(grid)
+    # the stop grid line is the line before the GRID STOP string
+    stopGridLine = lineSplitFile.index("GRID STOP")-1
+    # print(stopGridLine)
+    # print(lineSplitFile[stopGridLine])
 
-    #lasers=lineSplitFile.index("L")
-    #laserLines=list(map(str.find("L"),file))
+    # iterate the index from grid start line to grid end line plus 1 to include in range
+    gridString = lineSplitFile[startGridLine:stopGridLine+1]
 
-    #list of list storing the laser origin and tragectory
-    laserList=[]
-    #block list number list 
-    blockList=[]
-    #list of list storing the points the lasers must pass through
-    pointGoalList=[]
+    # define grid as an empty list of lists
+    grid = [[]]
 
-    #iterate from the end of the grid to the end of the file to avoid capturing values within the grid string
+    # lambda function below splits a given line into a list of values
+    def lineList(gridLine): return gridLine.split()
+
+    # print(lineList(gridString[0]))
+
+    # cast result of map function appied to gridString and assign as grid
+    grid = list(map(lineList, gridString))
+
+    # list of list storing the laser origin and tragectory, for now is initialized as an empty string to inform python that it has a job to do
+    laserList = []
+    # block type list
+    blockList = []
+    # list of list storing the points the lasers must pass through
+    pointGoalList = []
+
+    # iterate from the end of the grid to the end of the file to avoid capturing values within the grid string
     for line in lineSplitFile[stopGridLine:]:
+        # check for lazor trajectory
         if line.startswith("L"):
-            #take the current line, strip the leading L, then strip the leading whitespace, then split each integer value on the space, then append list to laserList
-            laserList.append(line.strip('L').strip().split())
-            print(laserList)
-
-            
+            # take the current line,
+            # strip the leading L,
+            # then strip the leading whitespace,
+            # then split each integer value on the space,
+            # then apply map function to cast to int
+            # then cast map output to list
+            # then append list to laserList
+            laserList.append(list(map(int, line.strip('L').strip().split())))
+        # check for point goals
         elif line.startswith("P"):
-            pointGoalList.append(line.strip('P').strip().split())
-            print(pointGoalList)
+            # take the current line,
+            # strip the leading P,
+            # then strip the leading whitespace,
+            # then split each integer value on the space,
+            # then apply map function to cast the string to an integer,
+            # then cast the map output to a list,
+            # then append this list to pointGoalList
+            thisLine = list(map(int, line.strip('P').strip().split()))
+            # pointGoalList.append(line.strip('P').strip().split())
+            pointGoalList.append(
+                list(map(int, line.strip('P').strip().split())))
+        # check for A blocks
+        elif line.startswith("A"):
+            # number of a blocks is assigned as the present line with the leading A striped, then with the leading spaces stripped, then cast to an integer value
+            blockNumA = int(line.strip('A').strip())
+            # iterate for extracted number of blocks
+            for i in range(blockNumA):
+                # append the global value for A blocks to the block list for as many times as the extracted blockNum indicates
+                blockList.append(REFLECTIVE)
+        # check for B blocks
+        elif line.startswith("B"):
+            # number of a blocks is assigned as the present line with the leading B striped,
+            # then with the leading spaces stripped,
+            # then cast to an integer value
+            blockNumB = int(line.strip('B').strip())
+            # iterate for extracted number of blocks
+            for i in range(blockNumB):
+                # append the global value for B blocks to the block list for as many times as the extracted blockNum indicates
+                blockList.append(REFRACTIVE)
+        # check for C blocks
+        elif line.startswith("C"):
+            # number of a blocks is assigned as the present line with the leading C striped,
+            # then with the leading spaces stripped,
+            # then cast to an integer value
+            blockNumC = int(line.strip('C').strip())
+            # iterate for extracted number of blocks
+            for i in range(blockNumC):
+                # append the global value for C blocks to the block list for as many times as the extracted blockNum indicates
+                blockList.append(OPAQUE)
 
-
-    #print(laserLines)
+    print(grid)
+    print(laserList)
+    print(pointGoalList)
+    print(blockList)
 
 # os.getcwd()
 file_path = os.getcwd() + '\dark_1.bff'
-#print(file_path)
+# print(file_path)
 openBFF(file_path)
