182d007299a0766968b0ac56dcbf5118bd1425d2
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-03-28 23:02:39 -0400

    [run] added LazorSolver objects for remaining test cases

--- run.py
+++ run.py
@@ -4,4 +4,7 @@ if __name__ == '__main__':
     LazorSolver('dark_1.bff')
     #LazorSolver('mad_1.bff')
     #LazorSolver('mad_4.bff')
-    LazorSolver('mad_7.bff')
+    #LazorSolver('mad_7.bff')
+    #LazorSolver('numbered_6.bff')
+    LazorSolver('showstopper_4.bff')
+    #LazorSolver('yarn_5.bff')
