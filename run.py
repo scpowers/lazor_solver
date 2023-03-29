347a5e5d6ae9e5aeb5e2e3060b4d897eed21d6ff
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-03-29 16:13:53 -0400

    [run] run file runs all given examples and times them

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from LazorSolver import LazorSolver
 
 if __name__ == '__main__':
     LazorSolver('dark_1.bff')
-    #LazorSolver('mad_1.bff')
-    #LazorSolver('mad_4.bff')
-    #LazorSolver('mad_7.bff')
-    #LazorSolver('numbered_6.bff')
+    LazorSolver('mad_1.bff')
+    LazorSolver('mad_4.bff')
+    LazorSolver('mad_7.bff')
+    LazorSolver('numbered_6.bff')
     LazorSolver('showstopper_4.bff')
-    #LazorSolver('yarn_5.bff')
+    LazorSolver('yarn_5.bff')
