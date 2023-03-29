ef5d5c9689c96b20e6653842b7f8adc45b2a0dcc
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2023-03-28 22:26:30 -0400

    [run] added LazorSolver object constructors for some test cases

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from LazorSolver import LazorSolver
 
 if __name__ == '__main__':
-    LazorSolver('bff/mad_1.bff')
+    LazorSolver('dark_1.bff')
+    #LazorSolver('mad_1.bff')
+    #LazorSolver('mad_4.bff')
+    LazorSolver('mad_7.bff')
